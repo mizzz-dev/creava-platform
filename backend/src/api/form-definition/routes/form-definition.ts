1a2c8dd9f3e3c54d9c8e4acbef5379f4012537a7
mizzz（ずーみー） <mizzz._.dev@outlook.jp>
2026-04-18 21:19:08 +0900

    Revert "Strapi起動時のform-definitionルーター初期化エラーを修正"

--- backend/src/api/form-definition/routes/form-definition.ts
+++ backend/src/api/form-definition/routes/form-definition.ts
@@ -2,25 +2,17 @@ import { factories } from '@strapi/strapi'
 
 const defaultRouter = factories.createCoreRouter('api::form-definition.form-definition')
 
-const publicRoutes = [
-  {
-    method: 'GET',
-    path: '/form-definitions/public',
-    handler: 'form-definition.publicList',
-    config: {
-      auth: false,
-    },
-  },
-]
-
 export default {
-  get prefix() {
-    return (defaultRouter as any).prefix
-  },
-  get routes() {
-    return [
-      ...((defaultRouter as any).routes ?? []),
-      ...publicRoutes,
-    ]
-  },
+  ...defaultRouter,
+  routes: [
+    ...(defaultRouter as any).routes,
+    {
+      method: 'GET',
+      path: '/form-definitions/public',
+      handler: 'form-definition.publicList',
+      config: {
+        auth: false,
+      },
+    },
+  ],
 }
